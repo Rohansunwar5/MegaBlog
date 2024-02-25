bd2b7d5f6a609e2a3504b24396ff10f121703b8e
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-02-25 22:48:01 +0530

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth';
+import { login,logout } from './store/authSlice'; 
+import { Outlet } from 'react-router-dom';
+import {Header, Footer} from "../src/components/index.js"
 
 function App() {
   const [loading, setLoading] = useState(true)
-  return(
-    <>
-    <h1>A mega blog app</h1>
-    </>
-  )
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() =>setLoading(false)) 
+  }, [])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-green-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO:TODO <Outlet/>
+        </main>
+        <Footer />
+      </div>
+    </div>
+
+  ) : (null) 
 }
 
 export default App
