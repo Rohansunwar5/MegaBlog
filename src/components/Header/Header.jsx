fc0f975387b1a28479689a8cc87ecd002f880f87
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-02-26 23:07:54 +0530

    Added components: Container, Logo, LogoutBtn. Updated Header and Footer components.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,42 @@
 import React from 'react'
+import {Container, Logo, LogoutBtn} from '../index'
+import { Link, useNavigate, useNavigation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
+// useSelector function helps use to acces state from the store 
 const Header = () => {
+  const authStatus = useSelector((state) => state.auth.status) // asking  for status of authentication in our application
+  const navigate = useNavigate();
+  // navigation is used for programmatic navigation, whenever this kind of naviagation is created, array is made and looped inside 
+  const naItems = [
+    {
+      name: 'Home',
+      slug: '/',
+      active: true,
+    },
+    {
+      name: 'Login',
+      slug: '/login',
+      active: !authStatus,
+    },
+    {
+      name: 'Signup',
+      slug: '/signup',
+      active: !authStatus,
+    },
+    {
+      name: 'All Posts',
+      slug: '/all-posts',
+      active: authStatus,
+    },
+    {
+      name: 'Add Post',
+      slug: '/add-post',
+      active: authStatus,
+    },
+  ]
+
+
   return (
     <div>Header</div>
   )
---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appwrite/configuration'
+import { logout} from '../../store/authSlice'
+
+function LogoutBtn() {
+    const dispatch = useDispatch()
+    const logoutHandler = () => {
+      authService.logout()
+      .then(() => {
+        dispatch(logout())
+      })
+    }
+  return (
+    <button>
+      Logout
+    </button>
+  )
+}
+
+export default LogoutBtn
