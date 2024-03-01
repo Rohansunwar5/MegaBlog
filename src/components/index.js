f275018e13529bf6cd185b856f9a9c65df905db4
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-03-01 23:47:54 +0530

    Add Signup, Login, Post, PostForm, PostCard compo

--- src/components/index.js
+++ src/components/index.js
@@ -4,9 +4,19 @@ import Container from "./container/Container.jsx"
 import Logo from './Logo'
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup.jsx"
+import Login from "./Login.jsx"
+import Post from "./Post";
+import PostForm from "./post-form/PostForm";
+import PostCard from './PostCard'
 
 export {
+  PostCard,
+  Post,
+  PostForm,
+  Login,
   RTE,
+  Signup,
   Header,
   Footer,
   Container,
